642c36461dd7cc34ea6dd679da0f8b784428dffb
darshand <darshan.bd@onze.in>
2017-02-23 10:49:51 +0530

    style change

--- Rails/how_to_generate_controller_without_assets_tests_view.rb
+++
@@ -1,7 +0,0 @@
-## We can generate controllers without assets tests or view templates while generating controllers.
-## This will be helpfull when generating controllers for api controllers.
-
-`rails g controller v2/[NAME] --no-helper --no-assets --no-template-engine --no-test-framework`
-
-> The above additional parameters skips the helper, assets, test and view default templates while generating controllers by default.
-
